452a61bdf84dabdbd220f53a56029a4dee065e93
Antony Rizzitelli <upd4ting@gmail.com>
2026-02-06 22:40:38 +0000

    refactor: use ioredis client

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,35 @@
 import { internal } from '@ajs.local/redis/beta';
-import { createClient, RedisClientOptions, RedisClientType } from 'redis';
+import Redis, { RedisOptions } from 'ioredis';
 
-let redisConfig: RedisClientOptions;
-export function construct(config: RedisClientOptions): void {
+export interface RedisConfig extends RedisOptions {
+  url?: string;
+}
+
+let redisConfig: RedisConfig;
+export function construct(config: RedisConfig): void {
   redisConfig = config;
 }
 
 export function destroy(): void {}
 
 export function start(): void {
-  const client = createClient(redisConfig) as RedisClientType;
-  void client.connect();
+  const client = createRedisClient(redisConfig);
   internal.SetClient(client);
 }
 
 export async function stop(): Promise<void> {
   const client = await internal.client;
-  await client.disconnect();
+  await client.quit();
   void internal.UnsetClient();
+}
+
+function createRedisClient(config: RedisConfig): Redis {
+  const { url, ...options } = config;
+  if (!url) {
+    return new Redis(options);
+  }
+  if (Object.keys(options).length === 0) {
+    return new Redis(url);
+  }
+  return new Redis(url, options);
 }
